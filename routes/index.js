70840f1191ff932d03f71757deda7778d50f0675
salamanderctesiphon <swbrookshire@gmail.com>
2023-07-29 14:44:24 -0500

    completed the route handlers

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'My App' });
+
+
+// GET home page.
+router.get("/", function (req, res) {
+  res.redirect("/catalog");
 });
+
+
+
 
 module.exports = router;
