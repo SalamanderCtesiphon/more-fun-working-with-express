e9e4c29367f16322e63b79cf7ce07b2bac62a5f2
salamanderctesiphon <swbrookshire@gmail.com>
2023-08-02 15:38:54 -0500

    trying to render the genres page

--- models/genre.js
+++ models/genre.js
@@ -6,11 +6,20 @@ const GenreSchema = new Schema({
   name: { type: String, required: true, maxLength: 100, minLength: 3 }
 });
 
-// Virtual for Genre's URL
+// Virtual for author's full name
+GenreSchema.virtual("genre").get(function () {
+  // To avoid errors in cases where an author does not have either a family name or first name
+  // We want to make sure we handle the exception by returning an empty string for that case
+  let genrename = this.genre_name;
+
+  return genrename;
+});
+
+// Virtual for author's URL
 GenreSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/genre/${this._id}`;
 });
 
 // Export model
-module.exports = mongoose.model("Gnere", GenreSchema);
+module.exports = mongoose.model("Genre", GenreSchema);
